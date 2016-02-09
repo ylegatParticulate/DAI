48dbcb5364f875c3e80929ab5531557d1c8c14b1
yessi <y.legat@gmx.de>
2016-02-09 18:51:44 +0100

    hg

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/santiago/Dropbox/Carrera/Quinto/Desarrollo_Aplicaciones_Internet/AprendiendoDjango/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
